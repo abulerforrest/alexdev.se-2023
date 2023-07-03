502c161d18f7c30056d5a001f2470d8274ac4c02
Alexander Forrest <alex@alexdev.se>
2023-07-03 16:41:56 +0200

    Fixed svg bug again..

--- src/func.ts
+++ src/func.ts
@@ -4,3 +4,11 @@ export const scrollToSection = (ref: MutableRefObject<null>) =>
   (ref.current as any)?.scrollIntoView({
     behavior: "smooth",
   });
+
+const IS_SERVER = typeof window === "undefined";
+export default function getURL(path: string) {
+  const baseURL = IS_SERVER
+    ? process.env.NEXT_PUBLIC_SITE_URL!
+    : window.location.origin;
+  return new URL(path, baseURL).toString();
+}
