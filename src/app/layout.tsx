2c78b58eb5b0b7611bd380f95ce5fd772384ac61
Alexander Forrest <alex@alexdev.se>
2023-07-03 20:14:41 +0200

    Fixed metadata, added to Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,45 @@
-"use client";
-import Head from "../components/atoms/Head";
+import { Metadata } from "next";
 import Home from "../components/templates/Home";
+
+export const metadata: Metadata = {
+  title: "alexdev.se",
+  description: "A portfolio site in multicolor - by Alexander Forrest.",
+  applicationName: "alexdev.se",
+  authors: [{ name: "Alexander Forrest", url: "https://alexdev.se" }],
+  keywords:
+    "development, challenge, opportunity, frameworks, code, Alexander Forrest, cartina, projects, javascript, typescript, webdev, tetris, resumé, connect, experience, agile, fullstack, alexdev, about, creative, frontend, front-end, build, user-friendly, seamless, UX, UI, design, webdesign, technical, skills, functional, programming, impression, vision, c++, php, ssh, bash, linux, terminal, responsive, innovative, explore, smart, portfolio, showcase, sleek, websites, applications, apps, ideas, passion, full-stack, figma, transforming, node js, next js, git, angular, redux, golang, deno, ts, material ui, ui, php, docker, vanilla, photoshop, wordpress",
+  generator: "Next.js",
+  referrer: "origin",
+  icons: ["https://alexdev.se/icon.png", "https://alexdev.se/favicon.ico"],
+  manifest: "https://alexdev.se/manifest.json",
+  openGraph: {
+    locale: "en",
+    countryName: "Sweden",
+    emails: "alex@alexdev.se",
+    url: "https://alexdev.se",
+    title: "alexdev.se",
+    description: "A portfolio site in multicolor - by Alexander Forrest.",
+    siteName: "alexdev.se",
+    images: [
+      {
+        url: "https://alexdev.se/icon.png",
+      },
+    ],
+  },
+  themeColor: "#FBFBF7",
+  colorScheme: "light",
+  creator: "Alexander Forrest",
+  publisher: "Alexander Forrest",
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow",
+  alternates: { canonical: "https://alexdev.se" },
+  twitter: {
+    card: "summary_large_image",
+    site: "@alexdev",
+    creator: "@zalzachipmusic",
+    images: "https://alexdev.se/icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,8 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <Head />
+    <html lang='en'>
       <body>
         <Home />
         {children}
--- src/components/atoms/Head.tsx
+++ src/components/atoms/Head.tsx
@@ -9,7 +9,7 @@ const Head = () => {
         content='A portfolio site in multicolor- by Alexander Forrest.'
       />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel='icon' type='image/png' href='/favicon.png' />
+      <link rel='icon' type='image/png' href='/favicon.png' sizes='any' />
       <link rel='preconnect' href='https://fonts.googleapis.com' />
     </NextHead>
   );
