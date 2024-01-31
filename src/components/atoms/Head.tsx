4e780206ee6c34d9b442489ca41079138d8761e6
Alexander Forrest <alex@alexdev.se>
2024-01-31 12:11:37 +0100

    added meta keywords

--- src/components/atoms/Head.tsx
+++ src/components/atoms/Head.tsx
@@ -7,7 +7,7 @@ const Head = () => {
       <link rel='icon' type='image/png' href='/favicon.png' sizes='any' />
       <link rel='preconnect' href='https://fonts.googleapis.com' />
 
-      <title>alexdev™ — Alex's Creative Portfolio In Multicolor</title>
+      <title>alexdev™ — Alex&apos;s Creative Portfolio In Multicolor</title>
       <meta
         name='title'
         content="alexdev™ — Alex's Creative Portfolio In Multicolor"
@@ -16,7 +16,10 @@ const Head = () => {
         name='description'
         content='Meet Alex, a full-stack developer crafting solid and responsive web solutions. Explore his portfolio — showcasing coding and creativity!'
       />
-
+      <meta
+        name='keywords'
+        content='development, challenge, opportunity, frameworks, code, Alexander Forrest, cartina, projects, javascript, typescript, webdev, tetris, resumé, connect, experience, agile, fullstack, alexdev, about, creative, frontend, front-end, build, user-friendly, seamless, UX, UI, design, webdesign, technical, skills, functional, programming, impression, vision, c++, php, ssh, bash, linux, terminal, responsive, innovative, explore, smart, portfolio, showcase, sleek, websites, applications, apps, ideas, passion, full-stack, figma, transforming, node js, next js, git, angular, redux, golang, deno, ts, material ui, ui, php, docker, vanilla, photoshop, wordpress'
+      />
       <meta property='og:type' content='website' />
       <meta property='og:url' content='https://alexdev.se' />
       <meta
